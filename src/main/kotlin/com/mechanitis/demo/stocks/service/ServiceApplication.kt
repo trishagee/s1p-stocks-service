d052367d9e217da61e5d00b859156099f3a6f51d
Trisha Gee <trisha.gee@gmail.com>
2019-09-20 11:10:55 +0200

    Introducing StockService

--- src/main/kotlin/com/mechanitis/demo/stocks/service/ServiceApplication.kt
+++ src/main/kotlin/com/mechanitis/demo/stocks/service/ServiceApplication.kt
@@ -3,6 +3,7 @@ package com.mechanitis.demo.stocks.service
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -18,16 +19,23 @@ fun main() {
 }
 
 @RestController
-class StockPricesRestController {
+class StockPricesRestController(private val stockService: StockService) {
 
     @GetMapping(value = ["/stocks/{symbol}"],
                 produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun prices(@PathVariable symbol: String): Flux<Double> {
+    fun prices(@PathVariable symbol: String) = stockService.streamOfPrices()
+
+}
+
+@Service
+class StockService {
+    fun streamOfPrices(): Flux<Double> {
         return Flux
                 .interval(Duration.ofSeconds(1L))
                 .map { randomStockPrice() }
     }
 
     private fun randomStockPrice() = ThreadLocalRandom.current().nextDouble(100.0)
+
 
 }
